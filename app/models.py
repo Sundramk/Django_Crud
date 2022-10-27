d726bd0e047a1db8d45da01bbc24a683f5cd15d7
sundram kumar <sundramkumar21102@gmail.com>
2022-10-27 17:39:01 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Student(models.Model):
+    Firstname = models.CharField(max_length=50)
+    Lastname = models.CharField(max_length=50)
+    Email = models.EmailField(max_length=50)
+    Contact = models.BigIntegerField()
